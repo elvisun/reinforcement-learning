675272d7ef1391affa8b5cea7dda5f5330a7d63a
Uriel Sade <urielsade@users.noreply.github.com>
2017-08-03 21:04:44 -0600

    Update replay_memory.py

--- dqn/replay_memory.py
+++ dqn/replay_memory.py
@@ -31,10 +31,6 @@ class ReplayMemory:
         if len(self.internal_memory) < self.MAX_CAPACITY:
             self.internal_memory.append(addition)
         else:
-            # i = 0
-            # while i < self.MAX_CAPACITY // 1000 and self.internal_memory[self.replace_index][2] > 0:
-            #     self.replace_index = (self.replace_index + 1) % self.MAX_CAPACITY
-            #     i += 1
             self.internal_memory[self.replace_index] = addition
             self.replace_index = (self.replace_index + 1) % self.MAX_CAPACITY
 
